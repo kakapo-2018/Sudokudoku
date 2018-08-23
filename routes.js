98b429e0b4bf260353b29a0b1db73b6ef64f9730
WinstonItiDev <winstonshred@gmail.com>
2018-08-24 01:42:57 +1200

    button and routes

--- routes.js
+++ routes.js
@@ -3,12 +3,11 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', (req,res) => {
-    let num = 0
-    if(num === 0) {
         res.render('home')
-    } else {
+})
+
+router.get('/sudoku', (req,res) => {
         res.render('sudoku')
-    } 
 })
 
 module.exports = router
