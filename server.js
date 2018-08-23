dc974bc2cc0652dc6344a781ea58e4a523d8edd6
student <yuzuki.sadoh@gmail.com>
2018-08-23 18:49:39 +1200

    added server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ server.engine('hbs', hbs({
   defaultLayout: 'main',
   extname: 'hbs'
 }))
+
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
