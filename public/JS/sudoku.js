13f475d0428d467c2f21af99f1889b4305880ecf
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-08-23 21:54:35 +1200

    adding easy sudoku

--- public/JS/sudoku.js
+++ public/JS/sudoku.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+let easySodokuAnswer = [
+    [8,4,6,9,3,7,1,5,2],
+    [3,1,9,6,2,5,8,4,7],
+    [7,5,2,1,8,4,9,6,3],
+    [2,8,5,7,1,3,6,9,4],
+    [4,6,3,8,5,9,2,7,1],
+    [9,7,1,2,4,6,3,8,5],
+    [1,2,7,5,9,8,4,3,6],
+    [6,3,8,4,7,1,5,2,9],
+    [5,9,4,3,6,2,7,1,8]
+]
+
+
+
+
 function startGame(){
     createBoard()
 }
@@ -27,17 +42,26 @@ function displayBoard (board) {
         for (var j = 0; j < 9; j++) {
 
             // Create a square
-            var div = document.createElement("div");
-            div.id = "r" + i + "c" + j;
-            div.style.width = squareSize + "px";
-            div.style.height = squareSize + "px";
-            div.style.border = "solid";
-            div.style.borderWidth = "1px"
-            div.style.borderColor = "black"
-            div.style.background = "white"
+            var input = document.createElement("input");
+            input.id = "r" + i + "c" + j;
+            input.style.width = squareSize + "px";
+            input.style.height = squareSize + "px";
+            input.style.border = "solid";
+            input.style.borderWidth = "1px"
+            input.style.borderColor = "black"
+            input.style.background = "white"
+            input.maxLength = "1"
 
             // Add square to board
-            document.getElementById("board").appendChild(div);
+            document.getElementById("board").appendChild(input);
+        }
+    }
+}
+
+function easySudoku(){
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            
         }
     }
 }
